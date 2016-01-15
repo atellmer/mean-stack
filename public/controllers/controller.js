348392a02aef01448cccb4bb7ea4f09e2a495bdb
AlexPlex <atellmer@gmail.com>
2016-01-16 01:49:26 +0500

    Use mongoJS and Add new row in database with POST

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -6,11 +6,23 @@ var app = angular.module('app',[]);
 app.controller('AppCtrl', ['$scope', '$http', function ($scope, $http){
 	console.log('Hello world from controller!');
 
-	$http.get('/contactlist').success(function(response){
-		console.log ('i got the data!');
+	var refresh = function(){
+		$http.get('/contactlist').success(function(response){
+			console.log ('i got the data!');
+			$scope.contactList = response;
+			$scope.contact = '';
+		});
+	}
 
-		$scope.contactList = response;
-	});
+	refresh();
 
+	$scope.addContact = function(){
+		//console.log($scope.contact);
+		$http.post('/contactlist', $scope.contact).success(function(response){
+			console.log(response);
+			refresh();
+		});
+
+	}
 
 }]);
--- server.js
+++ server.js
@@ -3,39 +3,33 @@
 var express = require('express');
 var app = express();
 
-/*
-app.get('/', function(req, res){
-	res.send('hello world from server node.js!');
-});
-*/
+var mongojs = require('mongojs');
+var db = mongojs('contactlist', ['contactlist']);
+
+var bodyParser = require('body-parser');
+
+
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
 app.get('/contactlist', function(req, res){
 	console.log('I received a GET request');
 
-	var person1 = {
-		name: 'Tim',
-		email: 'tim@mail.com',
-		number: '222-222-222'
-	};
-
-	var person2 = {
-		name: 'Emily',
-		email: 'emily@mail.com',
-		number: '333-333-333'
-	};
-
-	var person3 = {
-		name: 'John',
-		email: 'john@mail.com',
-		number: '444-444-444'
-	};
-
-	var contactList = [person1, person2 ,person3];
-
-	res.json(contactList);
+	db.contactlist.find({}, function (err, docs){
+			if (err) throw err;
+			console.log(docs);
+			res.json(docs);
+	});
 
 });
+
+app.post('/contactlist', function(req, res){
+		console.log('I recieved POST request!');
+		console.log(req.body);
+		db.contactlist.insert(req.body, function(err, doc){
+			res.json(doc);
+		})
+	});
 
 app.listen(3000);
 console.log('server is run on port 3000');

43e0017cc2730a604d3614dd14aee9827047ab66
AlexPlex <atellmer@gmail.com>
2016-01-16 00:36:12 +0500

    Use MongoDB

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -7,7 +7,7 @@ app.controller('AppCtrl', ['$scope', '$http', function ($scope, $http){
 	console.log('Hello world from controller!');
 
 	$http.get('/contactlist').success(function(response){
-		console.log ('i got the data');
+		console.log ('i got the data!');
 
 		$scope.contactList = response;
 	});

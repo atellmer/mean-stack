178d1f3f29abbe78a8d3590da99120373ae42728
AlexPlex <atellmer@gmail.com>
2016-01-15 22:34:54 +0500

    Set the exchange between the nodeJS and angularJS on HTTP

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -4,5 +4,13 @@ var app = angular.module('app',[]);
 
 
 app.controller('AppCtrl', ['$scope', '$http', function ($scope, $http){
-		console.log('Hello world from controller!');
+	console.log('Hello world from controller!');
+
+	$http.get('/contactlist').success(function(response){
+		console.log ('i got the data');
+
+		$scope.contactList = response;
+	});
+
+
 }]);
--- server.js
+++ server.js
@@ -10,5 +10,32 @@ app.get('/', function(req, res){
 */
 app.use(express.static(__dirname + '/public'));
 
+app.get('/contactlist', function(req, res){
+	console.log('I received a GET request');
+
+	var person1 = {
+		name: 'Tim',
+		email: 'tim@mail.com',
+		number: '222-222-222'
+	};
+
+	var person2 = {
+		name: 'Emily',
+		email: 'emily@mail.com',
+		number: '333-333-333'
+	};
+
+	var person3 = {
+		name: 'John',
+		email: 'john@mail.com',
+		number: '444-444-444'
+	};
+
+	var contactList = [person1, person2 ,person3];
+
+	res.json(contactList);
+
+});
+
 app.listen(3000);
 console.log('server is run on port 3000');

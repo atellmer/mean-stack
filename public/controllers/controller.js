a3fca6a4cc14ee1bd8e1bf124d5b7dd0778e653b
AlexPlex <atellmer@gmail.com>
2016-01-16 19:15:00 +0500

    RESTfull App finish

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -22,7 +22,36 @@ app.controller('AppCtrl', ['$scope', '$http', function ($scope, $http){
 			console.log(response);
 			refresh();
 		});
-
 	}
 
+	$scope.remove = function(id){
+		console.log(id);
+		$http.delete('/contactlist/' + id).success(function(response){
+			refresh();
+		});
+	}
+
+	$scope.edit = function(id){
+		console.log(id);
+		disable();
+		$http.get('/contactlist/' + id).success(function(response){
+			$scope.contact = response;
+		});
+	}
+
+	$scope.update = function(){
+		console.log($scope.contact._id);
+	
+		$http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function(response){
+			refresh();
+			enable();
+		});
+	}
+
+	var disable = function(){
+		$scope.add = true;
+	}
+	var enable = function(){
+		$scope.add = false;
+	}
 }]);
